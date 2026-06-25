0e722cd069c4d3ee91e0294ef59eac88d0dae652
Power366 <172065612+Power366@users.noreply.github.com>
2026-06-25 03:47:44 -0500

    add JWT refresh&access tokens for github callback add refreshToken to user schema

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,36 @@
 
 async function authCallbackGithub(req,res,next) {
-    res.json("send a token here instead")
+    try {
+        const userFound = req.user;
+        if(!userFound){
+            return res.redirect(`${process.env.FRONTEND_URL}/login?error=no_user`)
+        }
+        const accessToken= jwt.sign({id:userFound.id},
+        process.env.ACCESS_TOKEN_SECRET,
+        {expiresIn:"15m"});
+        const refreshToken= jwt.sign({id:userFound.id},
+        process.env.REFRESH_TOKEN_SECRET,
+        {expiresIn:"2d"});
+        await prisma.user.update({
+        where:{
+            id:Number(userFound.id)
+        },
+        data:{
+            refreshToken:refreshToken
+        }
+        })
+        res.cookie("jwt", refreshToken,{
+            secure: process.env.NODE_ENV ==="production",
+            httpOnly:true,
+            maxAge:2*24*60*60*1000,
+        });
+        return res.redirect(`${process.env.FRONTEND_URL}/auth-success?token=${accessToken}`)
+        res.json({accessToken:accessToken});
+
+        } catch (error) {
+            return res.redirect(`${process.env.FRONTEND_URL}/login?error=server_error`)
+        }
 }
 export default{
-    // loginGithub,
     authCallbackGithub
 }
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -7,7 +7,7 @@ authRotuer.get("/github/",
      passport.authenticate('github',{scope:['user:email'],session:false})
 )
 authRotuer.get("/github/callback",
-     passport.authenticate('github',{session:false,failureRedirect:"http://localhost:5173/error"}), 
+     passport.authenticate('github',{session:false,failureRedirect:`${process.env.FRONTEND_URL}/login?error=github_declined`}), 
     authController.authCallbackGithub);
 
 export default authRotuer;
